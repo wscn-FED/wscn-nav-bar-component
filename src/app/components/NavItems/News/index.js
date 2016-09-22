8a95c21502ef0f1e4a75ce59f93508ca6b4e8ce3
yuluyi <yuluyi1991@gmail.com>
2016-09-22 17:40:15 +0800

    news component

--- src/app/components/NavItems/News/index.js
+++ src/app/components/NavItems/News/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import withHover from '../../../utils/withHover';
 import withData from '../../../utils/withData';
+import Tabs, { TabPane } from '../../Tabs';
 
 @withHover
 export default
@@ -21,24 +22,74 @@ class News extends React.PureComponent {
 }
 
 
-@withData({
-        url: '/api/posts',
-        params: {
-            type: 'news',
-            limit: 3
-        },
-        method: 'get'
-    },
-    res => res.data
-)
-class NewsTab extends React.PureComponent {
+class ListView extends React.PureComponent {
     render() {
-        if (this.props.loading) return <span>'加载中...'</span>;
-        if (this.props.error) return <span>'出错了'</span>;
+        if (this.props.loading) return <div>加载中...</div>;
+        if (this.props.error) return <div>加载失败</div>;
+        const Item = this.props.itemComponent;
         return (
-            <div>
-                this.props.results
-            </div>
+            <ul>
+                {this.props.data.map((datum, index) => <Item key={this.props.getKey ? this.props.getKey(datum) : index} {...datum} />)}
+            </ul>
         );
     }
 }
+
+
+class Card extends React.PureComponent {
+    render() {
+        return (
+            <li>{this.props.title}</li>
+        );
+    }
+}
+
+
+const America = withData({
+    url: '/api/posts',
+    params: {
+        type: 'news',
+        limit: 3,
+        cid: 16
+    },
+    method: 'get'
+}, res => res.data.results)(ListView);
+
+const China = withData({
+    url: '/api/posts',
+    params: {
+        type: 'news',
+        limit: 3,
+        cid: 17
+    },
+    method: 'get'
+}, res => res.data.results)(ListView);
+
+const Europe = withData({
+    url: '/api/posts',
+    params: {
+        type: 'news',
+        limit: 3,
+        cid: 15
+    },
+    method: 'get'
+}, res => res.data.results)(ListView);
+
+class NewsTab extends React.PureComponent {
+    render() {
+        return (
+            <Tabs>
+                <TabPane name="美国">
+                    <America itemComponent={Card}/>
+                </TabPane>
+                <TabPane name="中国">
+                    <China itemComponent={Card}/>
+                </TabPane>
+                <TabPane name="欧洲">
+                    <Europe itemComponent={Card}/>
+                </TabPane>
+            </Tabs>
+        );
+    }
+}
+
