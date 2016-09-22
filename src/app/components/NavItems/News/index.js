a6cf20633f650cc97322af1f5071b01c5bbf4f63
yuluyi <yuluyi1991@gmail.com>
2016-09-22 16:39:22 +0800

    basic news component

--- src/app/components/NavItems/News/index.js
+++ src/app/components/NavItems/News/index.js
@@ -1,3 +1,44 @@
-export default function News() {
-    return null;
+import React from 'react';
+import withHover from '../../../utils/withHover';
+import withData from '../../../utils/withData';
+
+@withHover
+export default
+class News extends React.PureComponent {
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <span>资讯</span>
+                {
+                    this.props.hovered
+                    ? <NewsTab />
+                    : null
+                }
+            </div>
+        );
+    }
+}
+
+
+@withData({
+        url: '/api/posts',
+        params: {
+            type: 'news',
+            limit: 3
+        },
+        method: 'get'
+    },
+    res => res.data
+)
+class NewsTab extends React.PureComponent {
+    render() {
+        if (this.props.loading) return <span>'加载中...'</span>;
+        if (this.props.error) return <span>'出错了'</span>;
+        return (
+            <div>
+                this.props.results
+            </div>
+        );
+    }
 }
