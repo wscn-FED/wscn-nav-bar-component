95de5790dbd4dc6e7597be75672cef270fe20504
yuluyi <yuluyi1991@gmail.com>
2016-09-23 17:21:26 +0800

    added some optimization for withData

--- src/app/utils/withData.js
+++ src/app/utils/withData.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React from 'react';
 
+const MINIMUM_FETCH_INTERVAL = 5000;
+
 export default (config, map) => ComposedComponent => {
     class WithData extends React.Component {
         state = {
@@ -14,13 +16,23 @@ export default (config, map) => ComposedComponent => {
             this.mounted = true;
         }
 
+        componentWillReceiveProps() {
+            this.fetchData();
+        }
+
         componentWillUnmount() {
             this.mounted = false;
         }
 
+
         mounted = false;
 
         fetchData = () => {
+            if (this.latestFetchAt && Date.now() - this.latestFetchAt < MINIMUM_FETCH_INTERVAL) return;
+            this.setState({
+                loading: true
+            });
+            this.latestFetchAt = Date.now();
             axios(config)
             .then(res => {
                 if (this.mounted) {
