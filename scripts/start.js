aa9db477da92bcad346723832f8875bb3e190986
yuluyi <yuluyi1991@gmail.com>
2016-09-28 18:22:51 +0800

    added proxy

--- scripts/start.js
+++ scripts/start.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 process.env.NODE_ENV = 'development';
 
 var path = require('path');
@@ -116,6 +118,14 @@ function runDevServer(port) {
                 // ignorePath: true,
                 pathRewrite: {
                     '^/api': ''
+                },
+                changeOrigin: true,
+                logLevel: 'debug'
+            },
+            '/api-markets/**': {
+                target: 'http://api-markets.wallstreetcn.com/v1',
+                pathRewrite: {
+                    '^/api-markets': ''
                 },
                 changeOrigin: true,
                 logLevel: 'debug'
