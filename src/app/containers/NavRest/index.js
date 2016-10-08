bb987c1c9b71780f4079c3d3fc79d594680fe93b
yuluyi <yuluyi1991@gmail.com>
2016-10-08 19:10:38 +0800

    feat: added nav rest

--- src/app/containers/NavRest/index.js
+++ src/app/containers/NavRest/index.js
@@ -1,3 +1,20 @@
-export default function NavRest() {
-    return null;
+import React from 'react';
+import withDimension from '#/utils/withDimension';
+import './index.scss';
+
+@withDimension({once: true})
+export default
+class NavRest extends React.PureComponent {
+    static defaultProps = {
+        className: 'nav-rest'
+    }
+    render() {
+        console.log(this.props);
+        if (this.props.dimension.right && this.props.dimension.right > this.props.dimension.windowWidth) return <div className={this.props.className}>。。。</div>;
+        return (
+            <div className={this.props.className}>
+                {this.props.items.map(item => <div key={item.name}>{item.name}</div>)}
+            </div>
+        );
+    }
 }
