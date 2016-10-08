bf298bf877c0b104b781428da0efe26ea7799dd4
yuluyi <yuluyi1991@gmail.com>
2016-10-08 16:30:57 +0800

    feat: added nav item more

--- src/app/containers/NavItems/index.js
+++ src/app/containers/NavItems/index.js
@@ -1,9 +1,11 @@
 import News from './News';
 import Live from './Live';
 import Calendar from './Calendar';
+import More from './More';
 
 export default {
     News,
     Live,
-    Calendar
+    Calendar,
+    More
 };
