388330440d2bbc39e70556d6ecd406c663c228dc
yuluyi <yuluyi1991@gmail.com>
2016-10-10 14:43:30 +0800

    feat: added icon for hoverable

--- src/app/components/Hoverable/index.js
+++ src/app/components/Hoverable/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import withOpen from '#/HOC/withOpen';
 import classname, { withPrefix } from '#/utils/classnames';
+import Icon from '#/components/Icon';
 import './index.scss';
 
 @withOpen
@@ -14,7 +15,10 @@ class Hoverable extends React.PureComponent {
         const p = withPrefix(this.props.prefix);
         return (
             <div className={classname(p(), this.props.className)}>
-                <div className={p('name')}>{this.props.name}</div>
+                <div className={p('name')}>
+                    <span>{this.props.name}</span>
+                    <Icon symbolId="arrow-down" />
+                </div>
                 <div className={p('content')} data-open={this.props.open}>
                     {React.cloneElement(this.props.children, {open: this.props.open})}
                 </div>
