ce72cff44e17713ae604f0b862a94990de1cb205
yuluyi <yuluyi1991@gmail.com>
2016-10-10 14:43:09 +0800

    feat: adjust the animation of foldable

--- src/app/components/Foldable/index.js
+++ src/app/components/Foldable/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Motion, spring } from 'react-motion';
 import classname, { withPrefix } from '#/utils/classnames';
+import Icon from '#/components/Icon';
 import './index.scss';
 
 export default
@@ -23,7 +24,7 @@ class Foldable extends React.Component {
         const style = {
             indicator: {
                 opacity: spring(Number(this.state.fold)),
-                x: spring(Number(!this.state.fold))
+                x: spring(Number(!this.state.fold) * 10)
             },
             content: {
                 opacity: spring(Number(!this.state.fold))
@@ -33,10 +34,33 @@ class Foldable extends React.Component {
         return (
             <div className={classname(p(), this.props.className)}>
                 <Motion style={style.indicator}>
-                    {({x, opacity}) => <div onClick={this.unfold} className={p('indicator')} style={{transform: `translate(${x * 100}%)`, opacity}}><span>...</span></div>}
+                    {({x, opacity}) => (
+                        <div
+                            onClick={this.unfold}
+                            className={p('indicator')}
+                            style={{
+                                transform: `translate(${x * 100}%)`,
+                                opacity,
+                                visibility: this.state.fold ? 'visible' : 'hidden'
+                            }}
+                        >
+                            <Icon symbolId="dots" />
+                        </div>
+                    )}
                 </Motion>
                 <Motion style={style.content}>
-                    {({opacity}) => <div onClick={this.fold} className={p('content')} style={{ opacity }}>{this.props.children}</div>}
+                    {({opacity}) => (
+                        <div
+                            onClick={this.fold}
+                            className={p('content')}
+                            style={{
+                                opacity,
+                                visibility: !this.state.fold ? 'visible' : 'hidden'
+                            }}
+                        >
+                            {this.props.children}
+                        </div>
+                    )}
                 </Motion>
             </div>
         );
