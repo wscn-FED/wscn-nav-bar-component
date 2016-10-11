86e3247e583fab348afcaad8c809c4f2a8fba49b
yuluyi <yuluyi1991@gmail.com>
2016-10-11 19:12:56 +0800

    feat: added nav item search

--- src/app/containers/NavItems/index.js
+++ src/app/containers/NavItems/index.js
@@ -3,11 +3,13 @@ import Live from './Live';
 import Calendar from './Calendar';
 import More from './More';
 import Weex from './Weex';
+import Search from './Search';
 
 export default {
     News,
     Live,
     Calendar,
     More,
-    Weex
+    Weex,
+    Search
 };
--- src/app/containers/NavWidget/index.js
+++ src/app/containers/NavWidget/index.js
@@ -1,3 +1,21 @@
-export default function NavWidget() {
-    return null;
+import React from 'react';
+import NavItems from '#/containers/NavItems';
+import './index.scss';
+
+const { Search } = NavItems;
+
+export default
+class Widget extends React.Component {
+    static defaultProps = {
+        className: 'nav-widget'
+    }
+
+    render() {
+        return (
+            <div className={this.props.className}>
+                <Search />
+                <div>profile</div>
+            </div>
+        );
+    }
 }
