1f23444b3ebe384405e5ab0c04cbcfa445932be3
yuluyi <yuluyi1991@gmail.com>
2016-10-13 15:20:20 +0800

    feat: added nav-item-markets

--- src/app/containers/NavItems/Markets/index.js
+++ src/app/containers/NavItems/Markets/index.js
@@ -4,6 +4,67 @@ import Hoverable from '#/components/Hoverable';
 import Tabs, { TabPane } from '#/components/Tabs';
 import './index.scss';
 
+const WIDTH = 126;
+const HEIGHT = 20;
+
+class Chart extends React.PureComponent {
+    static defaultProps = {
+        className: 'chart'
+    }
+
+    getPoints() {
+        const values = this.props.data.map(([val]) => val);
+        const min = Math.min(...values);
+        const max = Math.max(...values);
+        const range = max - min;
+        return values.map((value, index) => {
+            const x = index / values.length * WIDTH;
+            if (range === 0) return `${x},${HEIGHT / 2}`;
+            const y = (max - value) / range * HEIGHT;
+            return `${x}, ${y}`;
+        });
+    }
+
+    render() {
+        return (
+            <svg className={this.props.className} viewBox={`-1 -1 ${WIDTH + 2} ${HEIGHT + 2}`}>
+                <polyline
+                    className="path"
+                    fill="none"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    stroke="currentColor"
+                    points={this.getPoints()}/>
+            </svg>
+        );
+    }
+}
+
+class Card extends React.PureComponent {
+    static defaultProps = {
+        className: 'markets-card'
+    }
+
+    render() {
+        const [ name, last, change, changeRate, precision ] = this.props.data[0];
+        return (
+            <div className={this.props.className} data-trend={change > 0 ? 'positive' : 'negative'}>
+                <div className="name">{name}</div>
+                <div className="value">{Number(last).toFixed(precision)}</div>
+                <div className="change">{change > 0 && '+'} {Number(change).toFixed(precision)}</div>
+                <div className="change-rate">{Number(changeRate).toFixed(precision)}%</div>
+                <Chart data={this.props.data[1]} />
+            </div>
+        );
+    }
+}
+
+function objToArr(obj) {
+    const { fields, ...rest } = obj; //eslint-disable-line
+    return Object.keys(rest).map(key => obj[key]);
+}
+
 class List extends React.PureComponent {
     static defaultProps = {
         className: 'markets-list'
@@ -20,7 +81,15 @@ class List extends React.PureComponent {
     render() {
         if (this.props.loading) return <div>加载中...</div>;
         if (this.props.error) return <div>加载失败</div>;
-        return <div>123</div>;
+        let data = this.props.data.map(datum => objToArr(datum));
+        data = data[0].map((x, i) => data.map(d => d[i]));
+        return (
+            <ul className={this.props.className}>
+                {data.map(datum => (
+                    <Card data={datum} key={datum[0][0]}/>
+                ))}
+            </ul>
+        );
     }
 }
 
