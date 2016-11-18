832383f6642019cfd084a895e280f3bedacc6514
Pan <panfree23@gmail.com>
2016-11-18 11:38:30 +0800

    fix time bug

--- src/app/containers/NavItems/Live/Card/index.js
+++ src/app/containers/NavItems/Live/Card/index.js
@@ -7,12 +7,12 @@ export default
 class Card extends React.PureComponent {
     static defaultProps = {
         className: 'live-card'
-    }
+    };
 
     render() {
         return (
             <li className={c(this.props.className)}>
-                <div className="live-card-time">{parseTime(this.props.createdAt, '{h}:{m}')}</div>
+                <div className="live-card-time">{parseTime(this.props.createdAt * 1000, '{h}:{i}')}</div>
                 <a href={this.props.isWeex ? '//strategy.weex.in/?from=wscnpc_narbar_livetab' : '//live.wallstreetcn.com'} target="_blank" rel="noopener noreferrer"><div className="live-card-title">{this.props.title}</div></a>
             </li>
         );
