3b8a91addd9c53eaa726ed683675dc2683b27cfd
Pan <panfree23@gmail.com>
2016-11-28 10:35:44 +0800

    下架社区 上专栏

--- src/app/containers/NavMain/index.js
+++ src/app/containers/NavMain/index.js
@@ -11,19 +11,22 @@ class NavMain extends React.PureComponent {
 
     mapConfigToItems(items) {
         return items.map(config => {
-            const { name, component } = config;
+            const {name, component} = config;
             const DynamicComponent = NavItems[component];
-            const el = React.createElement(DynamicComponent, {
-                key: name,
-                ...config
-            });
-            return el;
+            if (DynamicComponent) {
+                const el = React.createElement(DynamicComponent, {
+                    key: name,
+                    ...config
+                });
+                return el;
+            }
+            return <a className="nav-item-wrapper" href={config.href} key={name} target="_blank" rel="noopener noreferrer"><div className="nav-item">{name}</div></a>;
         });
     }
 
     render() {
         return (
-            <div className={c(this.props.className)} >
+            <div className={c(this.props.className)}>
                 {this.mapConfigToItems(this.props.items)}
             </div>
         );
